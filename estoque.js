39393375275bcde077cd8ee5339995e63e246477
João Srbek <joao.srbek@estudante.ufjf.br>
2023-12-05 21:44:01 -0300

    Cria e coloca para funcionar o atualizaTela

--- estoque.js
+++ estoque.js
@@ -1,12 +1,12 @@
 let estoque={
-    'joao': {
-        'maca': 1,
-        'banana': 2
-    },
-    'maria':{
-        'maca': 2,
-        'banana': 3
-    }
+    'joao': [
+        {'tipo': 'maca', 'qtd': 1},
+        {'tipo': 'pera', 'qtd': 2}
+    ],
+    'maria': [
+        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'banana','qtd': 4}
+    ]
 };
 
 function getEstoque(){
