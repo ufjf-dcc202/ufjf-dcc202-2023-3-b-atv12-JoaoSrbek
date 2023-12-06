fffa2496d053f3db4a2658b0901e6869c5bca6db
João Srbek <joao.srbek@estudante.ufjf.br>
2023-12-05 22:22:38 -0300

    cria função que permite que maria e joao doem frutas para o pomar

--- estoque.js
+++ estoque.js
@@ -2,10 +2,13 @@ let estoque={
     'joao': [
         {'tipo': 'maca', 'qtd': 1},
         {'tipo': 'pera', 'qtd': 2}
+
     ],
+
     'maria': [
         {'tipo': 'maca', 'qtd': 2},
         {'tipo': 'banana','qtd': 4}
+
     ]
 };
 
@@ -13,4 +16,16 @@ function getEstoque(){
     return structuredClone(estoque);
 }
 
-export{getEstoque};
+
+function transacao(origem, destino, tipo, quantidade) {
+    if(destino === "pomar"){
+        const pessoa = estoque[origem];
+        for(let i=0;i<pessoa.length;i++){
+            const monte = pessoa[i];
+            if(monte.tipo === tipo){
+                monte.qtd -= Math.min(quantidade, monte.qtd);
+            }
+        }
+    }
+} 
+export{getEstoque, transacao};
