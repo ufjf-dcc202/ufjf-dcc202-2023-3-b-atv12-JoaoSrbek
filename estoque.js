3429ee97dfbfa5f6ba5f5949306093fdcd8078e6
João Srbek <joao.srbek@estudante.ufjf.br>
2023-12-05 23:16:37 -0300

    corrige um teste

--- estoque.js
+++ estoque.js
@@ -1,14 +1,10 @@
 let estoque={
     'joao': [
         {'tipo': 'maca', 'qtd': 1},
-        {'tipo': 'pera', 'qtd': 2}
-
     ],
 
     'maria': [
         {'tipo': 'maca', 'qtd': 2},
-        {'tipo': 'banana','qtd': 4}
-
     ]
 };
 
